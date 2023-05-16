61d04f1c5b890fb8ede94da839729add08948775
LeNelSoN <nelis.valentin@gmail.com>
2023-05-16 15:45:29 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const App = () => (
+    <div>
+        <h1 className="test">
+            Hello World !
+        </h1>
+    </div>
+);
+
+export default App;
